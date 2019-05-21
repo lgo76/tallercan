f7d881e88efa4ab3fa9a311e06cd2bad233a62a4
Lucia Gomez <lgo76@alumnos.unican.es>
2019-05-21 19:42:59 +0200

    First commit

---
+++ src/main/java/es/tallercan/TallercanApplication.java
@@ -0,0 +1,13 @@
+package es.tallercan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallercanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallercanApplication.class, args);
+	}
+
+}
